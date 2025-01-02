a064a425d5b36494243653d3f2b8a00d8cd90e61
foxie-dev <Tina.saleh.888@gmail.com>
2025-01-02 17:39:23 +0100

    Initial Commit

---
+++ pia13swiftv4/pia13swiftv4App.swift
@@ -0,0 +1,17 @@
+//
+//  pia13swiftv4App.swift
+//  pia13swiftv4
+//
+//  Created by Elia Johannes on 2025-01-02.
+//
+
+import SwiftUI
+
+@main
+struct pia13swiftv4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
